539c8c32db81c879d2b333ab02392d87ec3d1892
Ciro Silva <80586272+ciro-silva@users.noreply.github.com>
2024-03-08 15:02:51 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+//SCROLLREVEAL
+
+ScrollReveal().reveal('.hero-left', {delay: 300});
+ScrollReveal().reveal('.hero-right', {delay: 400});
+ScrollReveal().reveal('.description-left', {delay: 500});
+ScrollReveal().reveal('.description-right', {delay: 600});
+ScrollReveal().reveal('.testimonials-text', {delay: 600});
+ScrollReveal().reveal('.container-testimonials-cards', {delay: 700});
+ScrollReveal().reveal('.testimonials-text', {delay: 700});
+ScrollReveal().reveal('.expert-left', {delay: 900});
+ScrollReveal().reveal('.expert-right', {delay: 1200});
+window.sr = ScrollReveal({reset: true})
